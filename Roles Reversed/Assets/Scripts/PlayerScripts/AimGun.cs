a7dfa3a070e0ac7651786b4cee1762f7bb29c8f4
Jacob Mosteller <46802543+JacobMosteller@users.noreply.github.com>
2023-07-07 17:46:05 -0600

    You can shoot now
     PEW PEW

--- Roles Reversed/Assets/Scripts/PlayerScripts/AimGun.cs
+++ Roles Reversed/Assets/Scripts/PlayerScripts/AimGun.cs
@@ -30,5 +30,21 @@ public class AimGun : MonoBehaviour
         Vector3 difference = target - player.transform.position;
         float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
         player.transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            float distance = difference.magnitude;
+            Vector2 direction = difference / distance;
+            direction.Normalize();
+            spawnShot(direction, rotationZ);
+        }
+    }
+
+    private void spawnShot(Vector2 direction, float rotationZ)
+    {
+        GameObject b = Instantiate(bulletPrefab) as GameObject;
+        b.transform.position = player.transform.position;
+        b.transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
+        b.GetComponent<Rigidbody2D>().velocity = direction * bulletSpeed;
     }
 }
