fcfb0c0bec3f0a84c2cee18cfffdadf210a08bf8
GrubSkrub <jaarmst25@gmail.com>
2023-07-08 19:47:05 -0600

    Homing Circle

---
+++ Roles Reversed/Assets/Scripts/EnemyScrips/Homing.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Homing : MonoBehaviour
+{
+
+    public Transform target;
+    public float speed = 5f;
+    public float rotateSpeed = 200f;
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start() {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        Vector2 direction = (Vector2)target.position - rb.position;
+        direction.Normalize();
+        float rotateAmount = Vector3.Cross(direction, transform.up).z;
+
+        rb.angularVelocity = -rotateAmount * rotateSpeed;
+
+        rb.velocity = transform.up * speed;
+    }
+
+    void OnTriggerEnter2D ()
+    {
+        Destroy(gameObject);
+    }
+}
